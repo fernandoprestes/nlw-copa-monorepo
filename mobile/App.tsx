5a3c4d8d157cb14909253308d69c4c3c8b2422ad
fernandoprestes <fernandoprestesc@gmail.com>
2022-11-02 17:39:29 -0300

    add context and auth google

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -8,7 +8,8 @@ import {
 import { THEME } from "./src/styles/theme";
 
 import { Loading } from "./src/components/Loading";
-import { SignIn } from "./src/screens/Signin";
+import { SignIn } from "./src/screens/SignIn";
+import { AuthContextProvider } from "./src/context/AuthContext";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -18,12 +19,14 @@ export default function App() {
   });
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      <AuthContextProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <SignIn /> : <Loading />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
--- mobile/src/screens/Signin.tsx
+++ mobile/src/screens/Signin.tsx
@@ -1,9 +1,27 @@
-import { Center, Text } from "native-base";
+import { Center, Text, Icon } from "native-base";
+import { Fontisto } from "@expo/vector-icons";
+import { useAuth } from "../hooks/useAuth";
+
+import Logo from "../assets/logo.svg";
+import { Button } from "../components/Button";
 
 export function SignIn() {
+  const { signIn, user } = useAuth();
+
   return (
-    <Center flex={1} bgColor="gray.900">
-      <Text color="#fff">Hello React Native</Text>
+    <Center flex={1} bgColor="gray.900" p={8}>
+      <Logo width={212} height={40} color="#F7DD43" />
+      <Button
+        my="8"
+        title="Entrar com o google"
+        variant="SECONDARY"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="sm" />}
+        onPress={signIn}
+      />
+      <Text textAlign="center">
+        Não utilizamos nenhuma informação além {"\n"} do seu e-mail para criação
+        de sua conta.
+      </Text>
     </Center>
   );
 }
